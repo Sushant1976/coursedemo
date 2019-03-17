2cc027efcbcb564540f82f731dde0b037e7ef7e3
Ubuntu <ubuntu@ip-172-31-35-31.us-east-2.compute.internal>
2019-03-17 17:19:49 +0000

    made my first change

--- Demo.java
+++ Demo.java
@@ -2,4 +2,4 @@ total 12
 drwxrwxr-x 3 ubuntu ubuntu 4096 Mar 17 17:06 .
 drwxr-xr-x 7 ubuntu ubuntu 4096 Mar 17 17:05 ..
 drwxrwxr-x 7 ubuntu ubuntu 4096 Mar 17 17:05 .git
--rw-rw-r-- 1 ubuntu ubuntu    0 Mar 17 17:06 Demo.java
+-bckkkabcbkkkdklrw-rw-r-- 1 ubuntu ubuntu    0 Mar 17 17:06 Demo.java
